332fe70678d21be621f4708dde5f0ce6a5fdb51b
vikra <vikramakam@hotmail.com>
2026-06-02 13:18:48 -0700

    moving_avg prog

--- linear_alg/moving_avg/moving_avg.cpp
+++ linear_alg/moving_avg/moving_avg.cpp
@@ -17,8 +17,35 @@ std::vector<float> create_y_vals(int size, float lower, float upper) {
     return arr;
 }
 
-std::vector<float> moving_avg(std::vector<float> arr) {
+bool not_in_middle(int i, int window_size) {
+    float half_window = window_size / 2;
+    float mid_window = ((i + half_window) + (i - half_window)) / 2;
 
+    return abs(i - mid_window) > 0.5; // accounts for even-sized windows
+}
+
+std::vector<float> moving_avg(std::vector<float> arr, int arr_size, int window_size) {
+    /*
+        Takes an array of values and averages them over the window_size.
+    */
+
+    std::vector<float> moving_avg_vals(arr_size);
+
+    for (int i = 0; i < arr_size; i++) {
+        /* 
+            handle edge cases where curr idx is not in the middle of where window is
+            (e.g. if window size 5, i = 1 or 2 (beginning of arr) or i = 99 or 100 (end))
+        */ 
+        if (i == 0 || i == arr_size - 1) { // at very beginning or very end, just set val to array val
+            moving_avg_vals[i] = arr[i];
+        } else {
+            // if somewhere within window but not in middle, set window upper bound to double that of i - lower bound
+            if (not_in_middle(i, window_size)) {
+                if ((i - window_size) - i)
+            }
+        }
+        
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -28,9 +55,15 @@ int main(int argc, char* argv[]) {
     }
 
     float lower = std::stof(argv[1]), upper = std::stof(argv[2]); // bounds of possible y values
+    int window_size = std::stoi(argv[3]);
 
     if (lower >= upper || abs(lower - upper) > 50.0) {
         std::cerr << "Bad lower and upper bound args; upper - lower > 50.0" << std::endl;
+        return 1;
+    }
+
+    if (window_size < 1) {
+        std::cerr << "Bad window size; window size must be > 1 units" << std::endl;
         return 1;
     }
 
@@ -43,7 +76,7 @@ int main(int argc, char* argv[]) {
 
     std::vector<float> y_vals = create_y_vals(size, lower, upper);
 
-    std::vector<float> moving_avg_vals = moving_avg(y_vals);
+    std::vector<float> moving_avg_vals = moving_avg(y_vals, size, window_size);
 
     
 
